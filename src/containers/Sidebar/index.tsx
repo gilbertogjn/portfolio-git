a4f6fe6978638a491d52835922eed4b0c99bcb77
Gilberto Garcia <gilbertogjn@gmail.com>
2023-12-12 15:18:01 -0400

    montando estrutura

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/gilbertogjn.png" alt="foto perfil" />
+    <Titulo fontSize={20}>GIlberto Garcia</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,16 @@
+import Titulo from '../../components/Titulo'
+import Paragrafo from '../../components/Paragrafo'
+
+const Sobre = () => (
+  <section>
+    <Titulo fontSize={16}>Sobre</Titulo>
+    <Paragrafo tipo="secundario">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis,
+      dolores? Nesciunt dolor dolorem consectetur est aliquam. Sint debitis
+      totam ipsam alias tempora molestiae sunt ullam illo soluta! Tempore,
+      maxime praesentium.
+    </Paragrafo>
+  </section>
+)
+
+export default Sobre
