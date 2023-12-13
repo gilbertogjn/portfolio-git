b25952939da176c81b822ecfff2b8bc97d3403be
Gilberto Garcia <gilbertogjn@gmail.com>
2023-12-13 16:47:12 -0400

    fix sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,14 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/gilbertogjn.png" alt="foto perfil" />
+    <Avatar />
     <Titulo fontSize={20}>GIlberto Garcia</Titulo>
+    <Paragrafo tipo="secundario" fontSize={16}>
+      gilbertogjn
+    </Paragrafo>
   </aside>
 )
 
