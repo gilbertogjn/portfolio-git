d6bdccc707a331225ed7ecdb2f60d6f3e8855f02
Gilberto <gilbertogjn@gmail.com>
2023-12-13 22:20:59 -0400

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -2,13 +2,21 @@ import Avatar from '../../components/Avatar'
 import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
 
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
+
 const Sidebar = () => (
   <aside>
-    <Avatar />
-    <Titulo fontSize={20}>GIlberto Garcia</Titulo>
-    <Paragrafo tipo="secundario" fontSize={16}>
-      gilbertogjn
-    </Paragrafo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>GIlberto Garcia</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        gilbertogjn
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
