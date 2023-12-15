c57b2ec855058d67d03f149f1035447319f526b1
Gilberto <gilbertogjn@gmail.com>
2023-12-15 10:01:38 -0400

    troca de tema

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
+import { ThemeProvider } from 'styled-components'
+
 import Projetos from './containers/Projetos'
 import Sidebar from './containers/Sidebar'
 import Sobre from './containers/Sobre'
 import EstiloGlobal, { Container } from './styles'
+import temaDark from './components/themes/dark'
+import temaLight from './components/themes/light'
+import { useState } from 'react'
 
 function App() {
+  const [usoTemaDark, setUsoTemaDark] = useState(false)
+
+  function trocaTema() {
+    setUsoTemaDark(!usoTemaDark)
+  }
+
   return (
-    <>
+    <ThemeProvider theme={usoTemaDark ? temaDark : temaLight}>
       <EstiloGlobal />
       <Container>
-        <Sidebar />
+        <Sidebar trocaTema={trocaTema} />
         <main>
           <Sobre />
           <Projetos />
         </main>
       </Container>
-    </>
+    </ThemeProvider>
   )
 }
 
--- src/containers/Sidebar/styles.ts
+++ src/containers/Sidebar/styles.ts
@@ -10,9 +10,9 @@ export const BotaoTema = styled.button`
   font-size: 10px;
   border-radius: 12px;
   border: none;
-  color: #eee;
+  color: ${(props) => props.theme.corFundo};
   font-weight: bold;
-  background-color: #282a35;
+  background-color: ${(props) => props.theme.corPrincipal};
   padding: 8px;
   cursor: pointer;
 `
@@ -20,4 +20,9 @@ export const SidebarContainer = styled.div`
   position: sticky;
   top: 80px;
   left: 0;
+
+  @media (max-width: 768px) {
+    margin-bottom: 40px;
+    text-align: center;
+  }
 `
